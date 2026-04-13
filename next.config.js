d68fac0a06deebef9778e68725fd8a4bbb8ff2be
Divyanshu-Dubey <divyanshudubey2712@gmail.com>
2026-04-13 21:23:58 +0530

    Fix Sass deprecation warnings for production build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const nextConfig = {
   reactStrictMode: false,
+  sassOptions: {
+    quietDeps: true,
+    silenceDeprecations: ['import', 'global-builtin', 'mixed-decls', 'color-functions']
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
